46a8b215f4f734c2a89302f73cbe4f31acdf9f48
Justin Bozonier <darkxanthos@gmail.com>
2009-03-26 23:00:16 -0700

    Split the single test into BDD style.

--- TwitDuel/TwitDuel.Specs/When_activated.cs
+++ TwitDuel/TwitDuel.Specs/When_activated.cs
@@ -6,15 +6,45 @@ using TwitDuel.Core;
 namespace TwitDuel.Specs
 {
     [TestFixture]
-    public class When_a_duel_start_message_is_received_from_twitter
+    public class When_a_duel_start_message_is_received_from_twitter : duel_start_with_one_message_waiting_context
     {
         [Test]
-        public void It_should_send_a_message_including_the_victim_and_aggressor()
-        {
-            var wasMessageSent = false;
-            var messageSent = "";
-            ContainerBootstrapper.BootstrapStructureMap();
+        public void It_should_send_a_message()
+        {   
+            Assert.IsTrue(WasMessageSent);
+        }
 
+        [Test]
+        public void It_should_include_the_victim()
+        {
+            Assert.IsTrue(MessageSent.Contains(VictimName));
+        }
+
+        [Test]
+        public void It_should_include_the_aggressor()
+        {
+            Assert.IsTrue(MessageSent.Contains(AggressorName));
+        }
+
+        [Test]
+        public void It_should_include_the_twitter_tag()
+        {
+            Assert.IsTrue(MessageSent.Contains(TwitterTag));
+        }
+
+        [Test]
+        public void It_should_be_under_the_twitter_size_limit()
+        {
+            Assert.IsTrue(MessageSent.Length < 140);
+        }
+
+        protected override void Because()
+        {
+            _Arena.ProcessLatestTweets();
+        }
+
+        protected override void Context()
+        {
             ObjectFactory.EjectAllInstancesOf<ITwitterDataAccess>();
             ObjectFactory.Inject<ITwitterDataAccess>(
                 new FakeDataAccess(() => new[]
@@ -22,24 +52,43 @@ namespace TwitDuel.Specs
                                                  new Tweet()
                                                      {
                                                          created_at = DateTime.Now,
-                                                         text = "@trvrtzn #ContestOfTwit",
-                                                         user = new TwitterUser() {screen_name = "@darkxanthos"}
+                                                         text = VictimName + " " + TwitterTag,
+                                                         user = new TwitterUser() {screen_name = AggressorName}
                                                      }
                                            },
-                                           message=>
-                                               {
-                                                   wasMessageSent = true;
-                                                   messageSent = message;
-                                                   return message;
-                                               }));
+                                           message =>
+                                           {
+                                               WasMessageSent = true;
+                                               MessageSent = message;
+                                               return message;
+                                           }));
+            _Arena = ObjectFactory.GetInstance<TweetArena>();
 
-            var arena = ObjectFactory.GetInstance<TweetArena>();
-
-            arena.ProcessLatestTweets();
-            Assert.IsTrue(wasMessageSent);
-            Assert.IsTrue(messageSent.Contains("@trvrtzn"));
-            Assert.IsTrue(messageSent.Contains("@darkxanthos"));
-            Assert.IsTrue(messageSent.Contains("#ContestOfTwit"));
         }
+
+        private bool WasMessageSent;
+        private string MessageSent;
+    }
+
+    public abstract class duel_start_with_one_message_waiting_context
+    {
+        protected TweetArena _Arena;
+        protected string VictimName = "@trvrtzn";
+        protected string AggressorName = "@darkxanthos";
+        protected string TwitterTag = "#ContestOfTwit";
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            ContainerBootstrapper.BootstrapStructureMap();
+
+            Context();
+            Because();
+
+        }
+
+        protected abstract void Because();
+
+        protected abstract void Context();
     }
 }
